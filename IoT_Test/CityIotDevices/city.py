1232a454387ca6172b4ff6308cfe24a318e851c2
leonh <leonh@FEINER-NB5-W10>
2017-03-13 02:23:25 +0100

    refactoring

--- IoT_Test/CityIotDevices/city.py
+++ IoT_Test/CityIotDevices/city.py
@@ -6,37 +6,7 @@ import logging
 import functools
 import operator
 
-import city_commands
-
-# This is a list of the active atributes of the iot devices
-# One tuple consist of a tuple and a list
-# ((object_id, object_name, type), [keylist])
-# object_id -- the ID being used for MQTT communication
-# object_name -- the name of the attribute in context broker (only used for creating an iot agent)
-# type -- the type of the value being stored in this attribute (only used for creating an iot agent)
-# keylist -- the list of keys to enter the nested dictionary of openweathermap for getting the value
-ATTRIBUTE_MATCHES = [(('t', 'Temperatur', 'Kelvin'),[u'main', u'temp']), 
-                     (('h', 'Humidity', '%'),[u'main', u'humidity']),
-                     (('w', 'Windspeed', 'mph'),[u'wind',u'speed']),
-                     (('p', 'Pressure', 'hPa'),[u'main',u'pressure']),
-                     (('c', 'Clouds', '%'),[u'clouds',u'all'])]
-
-
-# This is a list of the active atributes of the iot devices
-# One tuple consist of a tuple and a list
-# ((object_id, object_name, type), functionpointer)
-# object_id -- the ID being should for MQTT communication if NGSIv2 would be finished
-# object_name -- the name of the command in context broker (only used for creating an iot agent)
-# type -- the type of the value assotiated to this command (only used for creating an iot agent)
-# functionpointer -- a function wich should be carried out when command is called.
-#                    the function should be in in the folowing signature
-# retvalue = function(cityObject, paramvalue)
-# retvalue -- the value being pushed to context broker after finishing this command (use dummy if not required)
-# cityObject -- the city which is ordered to carry out the commmand (use dummy if not required)
-# paramvalue -- the value being handed over by context broker when triggering the command (use dummy if not required)
-COMMAND_MATCHES = [(('l','CpuLoad', '%'), city_commands.get_cpu_load), (('u', 'UpdateCount','int'), city_commands.get_update_count)]
-
-SERVER = '130.206.112.29'  # The IP of the MQTT, IOT and the Context Broker API
+import city_definitions
 
 class city(object):
     """
@@ -45,30 +15,31 @@ class city(object):
     One instance needs its own mqtt iot agent and does not know anything about context broker
     When calling the method update the city is pulling information from OpenWeatherMap, extracts the desired information and forwards it trough mqtt.
     """
-    MQTT_API_KEY = 1234  # The apikey for using the MQTT API
-    OWM_KEY = 'abacdae90c8800879db8e02327da1f92' # The apikey for using the openweathermap API
-    OWM_URL = 'http://api.openweathermap.org/data/2.5/weather?id={}&appid={}' # The URL of the openweathermap API
-    MQTT_SERVER = SERVER # The IP of the MQTT API 
-    MQTT_PORT = 1883 # The Port of the MQTT API 
-    DEVICE_BASISNAME = 'DevID37101' # The first part of the names of Devices in MQTT API 
+    MQTT_API_KEY = city_definitions.MQTT_API_KEY  # The apikey for using the MQTT API
+    OWM_KEY = city_definitions.OWM_KEY # The apikey for using the openweathermap API
+    
+    MQTT_SERVER = city_definitions.SERVER # The IP of the MQTT API 
+    MQTT_PORT = city_definitions.MQTT_PORT # The Port of the MQTT API 
+    DEVICE_BASISNAME = city_definitions.DEVICE_BASISNAME_MQTT # The first part of the names of Devices in MQTT API 
 
     # The list of active attributes consists of tuples. The tuple contain the name of the attribute in MQTT API
     #  and the the list of keys to enter the nested dictionary of openweathermap for getting the value
-    ACTIVE_ATTRIBUTES = [(name_mqtt, path_owm) for ((name_mqtt, _, _), path_owm) in ATTRIBUTE_MATCHES] 
+    ACTIVE_ATTRIBUTES = city_definitions.ACTIVE_ATTRIBUTES
 
     # The list of commands consists of tuples. The tuple contain the name of the attribute in context broker api (NGSIv2 not ready)
     #  and pointer to the function which should be called when the command is triggerd.
-    COMMANDS = { key : value for ((_ , key , _ ), value) in COMMAND_MATCHES }
+    COMMANDS = city_definitions.COMMANDS
 
+    OWM_URL = 'http://api.openweathermap.org/data/2.5/weather?id={}&appid={}' # The URL of the openweathermap API
 
-    def __init__(self, cityOwmId, iata):
+    def __init__(self, city_owm_id, iata):
         """
         Constructor of city
         Keyword arguments:
         cityOwmId -- the city id of openweathermap
         iata -- the IATA code of the city
         """
-        self.city_owa_id = cityOwmId # The id of the city in openweathermap
+        self.city_owm_id = city_owm_id # The id of the city in openweathermap
         self.device_id = city.DEVICE_BASISNAME + iata # the id of the city in MQTT
         #self.iata = iata
         self.mqtt_client = mqtt.Client()
@@ -97,7 +68,7 @@ class city(object):
         Returns:
         Nested dictionaries with values of the weather of the city
         """
-        r = requests.get(city.OWM_URL.format(self.city_owa_id, city.OWM_KEY))
+        r = requests.get(city.OWM_URL.format(self.city_owm_id, city.OWM_KEY))
         return r.json() if 200 == r.status_code else None
     
     @staticmethod
@@ -171,144 +142,7 @@ class city(object):
             json.dumps({command_name : tryApply(command_name, command_value) for command_name, command_value in dict_mqtt_json.items() if command_name in city.COMMANDS}))
 
     def __repr__(self):
-        return str((self.city_owa_id, self.device_id))
+        return str((self.city_owm_id, self.device_id))
 
     def __str__(self):
-        return 'IOT Name = {}, OWA City ID = {}'.format(self.device_id, self.city_owa_id)
-
-
-def createCity(owa_id, iata, city_name = None):
-    """
-    Create a city object and the assotiated IOT agent.
-    Keyword arguments:
-    cityOwmId -- the city id of openweathermap
-    iata -- the IATA code of the city
-    cityName (optional) -- The name of the city being stored in iot agent. Default is the name being stored in openweathermap.
-    Returns:
-    The created city object
-    """
-    REPLACE_ENTITY = True
-    FIWARE_SERVICE_PATH = '/environment'
-    FIWARE_SERVICE = 'icai31701'
-    IOT_SERVER = SERVER
-    IOT_PORT = 5050
-    CONTEXTBROKER_SERVER = SERVER
-    CONTEXTBROKER_PORT = 1026
-    IOT_AGENT_URL = 'http://{}:{}/iot/devices/'.format(IOT_SERVER, IOT_PORT)
-    CONTEXTBROKER_URL = 'http://{}:{}/v2/entities/'.format(CONTEXTBROKER_SERVER,CONTEXTBROKER_PORT)
-    
-    logger = logging.getLogger(__name__)
-
-    theCity = city(owa_id,iata)
-
-
-    # get some information for the static attributes associated to this city from the from openweathermap
-    dict_owm_json = theCity.get_owm_json()
-    latitude = dict_owm_json['coord']['lat']
-    longitude = dict_owm_json['coord']['lon']
-    if not city_name:
-        city_name = dict_owm_json['name']
-    country = dict_owm_json['sys']['country']
-        
-    static_attributes = [('Position', 'geo:point',"{},{}".format(latitude, longitude)), ('Countrycode', 'string', country), ('Name', 'string', city_name)]
-    name_contextbroker =  "DevEnt{}".format(iata)
-    name_mqtt = "DevID37101{}".format(iata)
-    
-    device = { "device_id" : name_mqtt, 
-                "entity_name" : name_contextbroker, 
-                "entity_type" : "Device", 
-                "transport" : "MQTT",
-                "attributes" : [{ "object_id" : a_id, "name" : a_name, "type" : a_type } for ((a_id, a_name, a_type), _ ) in ATTRIBUTE_MATCHES],
-                "commands" : [{ "object_id" : a_id, "name" : a_name, "type" : a_type } for ((a_id, a_name, a_type) , _ ) in COMMAND_MATCHES],
-                "static_attributes" : [{ "name" : a_id, "type" : a_name, "value" : a_type } for a_id, a_name, a_type in static_attributes]  
-            }
-
-    device_list = {"devices" : [device]}
-
-    # check wether city iot agent already exists. Delete if yes.
-    response = requests.get(IOT_AGENT_URL + name_mqtt, headers = {'Fiware-Service': FIWARE_SERVICE, 'Fiware-ServicePath' : FIWARE_SERVICE_PATH})
-    if 200 == response.status_code:
-        logger.warning ("{}: IOT agent does already exist. Deleting IOT agent!".format(iata))
-        response = requests.delete(IOT_AGENT_URL + name_mqtt, headers = {'Fiware-Service': FIWARE_SERVICE, 'Fiware-ServicePath' : FIWARE_SERVICE_PATH})
-        if 204 == response.status_code:
-            logger.info ("{}: IOT agent successfully deleted".format(iata))
-        else:
-            logger.error("{}: IOT agent could not be deleted. Http error code: {}".format(iata, response.status_code)) 
-
-
-    # check wether city entity already exists. Delete if yes and REPLACE_ENTITY is true.
-    response = requests.get(CONTEXTBROKER_URL + name_contextbroker, headers = {'Fiware-Service': FIWARE_SERVICE, 'Fiware-ServicePath' : FIWARE_SERVICE_PATH})
-    if 200 == response.status_code:
-        if REPLACE_ENTITY:
-            logger.warning ("{}: Entity does already exist. Deleting entity!".format(iata))
-            response = requests.delete(CONTEXTBROKER_URL + name_contextbroker, headers = {'Fiware-Service': FIWARE_SERVICE, 'Fiware-ServicePath' : FIWARE_SERVICE_PATH})
-            if 204 == response.status_code:
-                logger.info ("{}: Entity successfully deleted".format(iata))
-            else:
-                logger.error("{}: Entity could not be deleted. Http error code: {}".format(iata, response.status_code)) 
-        else:
-            logger.info ("{}: Entity does already exist.".format(iata))
-
-
-    # create the iot Agent
-    logger.info("{}: Create IOT agent.".format(iata))
-    response = requests.post(IOT_AGENT_URL, json = device_list,
-                    headers = {'Fiware-Service': FIWARE_SERVICE, 'Fiware-ServicePath' : FIWARE_SERVICE_PATH})
-    if 201 == response.status_code:
-        logger.info ("{}: IOT agent has been created!".format(iata))
-    else:
-        logger.error("{}: IOT agent could not be created. Http error code: {}".format(iata, response.status_code)) 
-
-    # update the context broker
-    logger.info("{}: Update city".format(iata))
-    if theCity.put_mqtt_json(city.convert_owm_to_mqtt(dict_owm_json)):
-        logger.info("{}: Update sucessfully finished".format(iata))
-    else:
-         logger.error("{}: city could not be updated".format(iata))
-
-    return theCity
-
-def getCityOwmIdAndIata(name):
-    """
-    Gets the city id of openweathermap and the IATA code of the city
-    Returns a tuple of:
-    cityOwmId,iata -- the city id of openweathermap, the IATA code of the city
-    """
-    logger = logging.getLogger(__name__)
-    
-    # get the IATA code
-    IATACODES_KEY = '32320f7b-58ee-46a7-847e-468b451eb083'
-    r = requests.get('http://api.openweathermap.org/data/2.5/weather?q={}&appid={}'.format(name, city.OWM_KEY))
-    owa_id = r.json()['id']
-    owaName = r.json()['name']
-
-    # get the city id of openweathermap
-    r = requests.get('https://iatacodes.org/api/v6/autocomplete?query={}&api_key={}'.format(name, IATACODES_KEY))
-    cities = r.json()['response']['cities']
-    iata = cities[0]['code']
-    iataName = cities[0]['name']
-    logger.info ('IATA: {}, OWM_ID: {},'.format(owa_id,iata))
-    if (not(name == owaName == iataName)):
-        logger.warning('The names are different! Name: {}, OWM_Name: {}, IATA_Name: {}'.format (name, owaName, iataName))
-    return (owa_id,iata)
-
-def createCityByName(name):
-    """
-    Create a city object and the assotiated IOT agent.
-    Keyword arguments:
-    cityName -- The name of the city being stored in iot agent and found it openweathermap and in IATA database.
-    Returns:
-    The created city object
-    """
-    (cityOwmId,iata) = getCityOwmIdAndIata(name)
-    return createCity(cityOwmId, iata, name)
-    
-def getCityByName(name):
-    """
-    Create a city object. The IOT agent must already exist.
-    Keyword arguments:
-    cityName -- The name of the city being stored in iot agent and found it openweathermap and in IATA database.
-    Returns:
-    The created city object
-    """
-    return city(*getCityOwmIdAndIata(name))
+        return 'IOT Name = {}, OWA City ID = {}'.format(self.device_id, self.city_owm_id)
--- IoT_Test/CityIotDevices/main.py
+++ IoT_Test/CityIotDevices/main.py
@@ -3,17 +3,18 @@ import time
 import logging
 
 import city
+import city_creators
 import city_scheduler
 
 logging.basicConfig(level=logging.INFO)
 
 # two ways to create city object
-#muc = city.city(cityOwmId = 6940463, cityId = 'MUC') 
-#muc = city.getCityByName('Munich')
+#muc = city.city(city_owm_id = 6940463, iata = 'MUC') 
+#muc = city_creators.get_city_by_name('Munich')
 
 # two ways to create city object and iot Agent
-#muc = city.createCity(cityOwaId = 6940463, cityId = 'MUC', cityName = 'Munich')
-#muc = city.createCityByName('Munich') #create city object and iot Agent
+#muc = city_creators.create_city(city_owm_id = 6940463, iata = 'MUC', city_name = 'Munich')
+#muc = city_creators.create_city_by_name('Munich') #create city object and iot Agent
 
 #print (muc)
 
@@ -21,12 +22,16 @@ logging.basicConfig(level=logging.INFO)
 #pair_owmid_iata_list = [(6940463, 'MUC'), (3117735, 'MAD'), (5128581, 'NYC'),  (1796236, 'SHA')]
 #city_list = ([city.city(*pair_owmid_iata) for pair_owmid_iata in pair_owmid_iata_list]
 
-
+#create and delete a city
+#city_creators.create_city_by_name('Singapore')
+#city_creators.delete_iot_agent('SIN')
+#city_creators.delete_contextbroker_entity('SIN')
 
 # create city with or without iot agent by an list of city names
 city_name_list = ['Berlin', 'Munich', 'New York', 'Shanghai', 'Madrid', 'Sydney', 'Cape Town']
-city_list = [city.getCityByName(name) for name in city_name_list] 
-#city_list = [city.createCityByName(name) for name in city_name_list] 
+city_list = [city_creators.get_city_by_name(name) for name in city_name_list] 
+#city_list = [city_creators.create_city_by_name(name) for name in city_name_list] 
 
 #start updating the cities constantly
 city_scheduler.repeat(1.0/6, *city_list)
+
